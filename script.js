7a40813768d7deb9a5c5238a9a7d06b252b07309
drsaffas <81782489+drsaffas@users.noreply.github.com>
2022-10-25 11:04:12 +0000

    A hot fix!

--- script.js
+++ script.js
@@ -2,34 +2,31 @@ var newLevel = 1;
 var progress = 2;
 var point = 0;
 
-function guessNumber(x, y, z) {
-    const random = Math.floor(Math.random() * y) + 1;
-    var msg1 = "Hello " + name + "!\nWelcome to level " + x + "\nYour point is " + z + " Now!\nGuess a number from 1 to " + y + " : ";
-    var msg2 = "Ooops, " + name + "try again!\nYou're still in level " + x + ".\nAnd your point is still " + z + ".\nGuess a number from 1 to " + y " : ";
-    
+function guess() {
+    const random = Math.floor(Math.random() * progress) + 1;
+    var msg1 = "Hello " + name + "!\n\nWelcome to level " + newLevel + ". Your point is " + point + " now!\n\nGuess a number from 1 to " + progress + " : ";
+    var msg2 = "Ooops, try again " + name + "!\n\nYou're still in level " + newLevel + ". And your point is still " + point + ".\n\nGuess another number from 1 to " + progress + " : ";
     let number = parseInt(prompt(msg1));
     
     while(number !== random) {
         number = parseInt(prompt(msg2));
     }
-    
     if (number == random) {
-        x++;
-        y++;
-	z = z + 5;
-        var result = prompt("Congratulations " + name + " You win!\n" + "\nYou also receive additional 5 points. Your point now is " + z + ".\nDo you want to continue to the next level? Please type yes to continue!");
-	
+        newLevel++;
+        progress++;
+        point += 5;
+        var result = prompt("Congratulations " + name + "!\n\nYou guessed the correct number and you received additional points. Your current point is " + point + ".\n\nDo you want to continue to the next level? Please type yes to continue!");
         if (result == "yes") {
-            guessNumber(x, y, z);
+        guess();
 }
         else {
-            alert("Thanks for playing number guessing game by Dr. Saffas!");
+            alert("Thanks " + name + "! for playing number guessing game by Dr. Saffas!");
         }
     }
 }
 
-alert("You're welcome to number guessing game. Written by Dr. Saffas! Please press OK to start!");
+alert("You're welcome to number guessing game. Created by Dr. Saffas! Please press OK to start!");
 
-var name = prompt("Please, enter your name first!")
+var name = prompt("What's your name? ");
 
-guessNumber(newLevel, progress, point);
+guess();
